d4d787dcbe9e16b33336b85829596b61da04b2f6
gustavo23gus <148358700+gustavo23gus@users.noreply.github.com>
2026-03-30 19:51:29 -0300

    Delete api_eventos/api directory

--- api_eventos/api/status.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res){
-    res.status (200).json({
-        status: 'ok',
-        sistema: "API de Comunicados de eventos",
-        ambiente: "cloud"
-    })
-    }
-    
